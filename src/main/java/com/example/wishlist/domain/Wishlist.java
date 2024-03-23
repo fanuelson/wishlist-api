15ba647d79d2ca033c6720f7d7611732a8499154
Fanuel Nunes <fnunes@testreach.com>
2024-03-22 22:20:31 -0300

    refact

--- src/main/java/com/example/wishlist/domain/Wishlist.java
+++ src/main/java/com/example/wishlist/domain/Wishlist.java
@@ -5,8 +5,6 @@ import lombok.Builder;
 import lombok.Data;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
